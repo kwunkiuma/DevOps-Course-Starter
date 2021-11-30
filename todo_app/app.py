048eb237f1aa2099821416fef54e81850767378e
Frank Ma <frank.ma@softwire.com>
2021-11-30 17:41:40 +0000

    Review markups

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items import *
+from todo_app.data.session_items import add_item, get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
